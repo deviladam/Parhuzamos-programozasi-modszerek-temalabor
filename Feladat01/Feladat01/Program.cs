bffd0e18a10494e25f6917a4e4bd9abc570d8ad5
deviladam <szekely.adam.llg@gmail.com>
2018-09-24 18:49:24 +0200

    initialize A and B matrix

--- Feladat01/Feladat01/Program.cs
+++ Feladat01/Feladat01/Program.cs
@@ -10,6 +10,20 @@ namespace Feladat01
 	{
 		static void Main(string[] args)
 		{
+			const int rngMax = 1000;		//legnagyobb random szám 
+			Random random = new Random();
+			int size = int.Parse(args[0]);	//mátrixok mérte 
+			int[,] mxA = new int[size, size];	//matrix A
+			int[,] mxB = new int[size, size];    //matrix B
+			int[,] mxAB = new int[size, size];	//matrix A*B
+			for (int i = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					mxA[i,j] = random.Next(rngMax);
+					mxB[i,j] = random.Next(rngMax);
+				}
+			}
 		}
 	}
 }
